32821f589cf11236212dca3edd44aae0266eddd1
hirabayashi-tatsuo <t-hirabayashi@ne-os.jp>
2025-12-02 14:52:35 +0900

    Fix app SG ingress/egress and HTTP rule for pj3

--- pj3-terraform/main.tf
+++ pj3-terraform/main.tf
@@ -49,20 +49,20 @@ resource "aws_security_group" "app_sg" {
     security_groups = [var.bastion_sg_id]
   }
 
-#  ingress {
-#    description = "HTTP for test"
-#    from_port   = 80
-#    to_port     = 80
-#    protocol    = "tcp"
+  ingress {
+    description = "HTTP for test"
+    from_port   = 80
+   to_port     = 80
+   protocol    = "tcp"
+   cidr_blocks = ["0.0.0.0/0"]
+  }
+
+#  egress {
+#    from_port   = 0
+#    to_port     = 0
+#    protocol    = "-1"
 #    cidr_blocks = ["0.0.0.0/0"]
 #  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
   tags = merge(local.common_tags, {
     Name = "${var.project_name}-app-sg"
